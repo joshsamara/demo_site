d4791323f8459b0621a37fd42c108b6bf12c68e7
Josh Samara <joshsamara@gmail.com>
2015-01-31 20:51:58 -0500

    Fix everything

--- mysite.py
+++ mysite.py
@@ -3,7 +3,18 @@ import os
 
 
 class MySite(object):
-    pass
+    @cherrypy.expose
+    def index(self):
+        return """ <html>
+    <head>
+        <title>Cat Hats</title>
+    </head>
+    <html>
+    <body>
+        <h1>Cat Hats</h1>
+        <img src="http://i.imgur.com/ogXW5uf.jpg">
+    </body>
+    </html>"""
 
 if __name__ == '__main__':
     cherrypy.config.update({'server.socket_host': '0.0.0.0', })
